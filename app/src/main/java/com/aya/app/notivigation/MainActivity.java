385bc846ae27b6ca47e1089433815a7b126e52ef
AyaOsamaAhmed <37303559+AyaOsamaAhmed@users.noreply.github.com>
2019-05-02 15:49:34 +0200

    Initial commit

---
+++ app/src/main/java/com/aya/app/notivigation/MainActivity.java
@@ -0,0 +1,68 @@
+package com.aya.app.notivigation;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//https://developer.android.com/training/notify-user/build-notification
+public class MainActivity extends AppCompatActivity {
+
+    private static final String CHANNEL_ID = "1";
+    private int notificationID = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //make chanel notification if sdk 8.0.0
+        createNotificationChannel();
+        //create notification
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this , CHANNEL_ID);
+        mBuilder.setSmallIcon(R.drawable.notivication);
+        mBuilder.setContentTitle("Notification Alert, Click Me!");
+        mBuilder.setContentText("Hi, This is Android Notification Detail!");
+
+        //make intent when click on it
+        Intent resultIntent = new Intent(this, MainActivity.class);  // move to
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(resultPendingIntent);
+
+        // To be notification appear
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(notificationID, mBuilder.build());
+
+
+    }
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+
+
+}
